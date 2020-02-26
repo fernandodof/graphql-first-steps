4898ce4a83122be8d174676e31db15cb06a9027b
Fernando Ferreira <fernandodof@gmail.com>
2020-02-26 21:15:07 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const fs = require('fs');
+const express = require('express');
+const db = require('./database');
+
+const port = 3000;;
+
+const app = express();
+
+const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf-8' }));
+const resolvers = require('./resolvers');
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+app.listen(port, () => console.info(`Server started on port ${port}`));
