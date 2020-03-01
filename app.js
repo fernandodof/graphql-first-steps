718c15de3cd30886876bf12f52ab20d6e4735f9f
Fernando Ferreira <fernandodof@gmail.com>
2020-03-01 18:03:13 +0100

    creating more resolvers and refining schema

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const fs = require('fs');
 const express = require('express');
 
 const port = 3000;;
-
 const app = express();
 
+// Build a schema using GraphQL schema language from the schema file 
 const typeDefs = gql(fs.readFileSync('./schema.graphql', { encoding: 'utf-8' }));
+
+// Resolvers functions. These functions are basically what our api will call to retrieve data
 const resolvers = require('./resolvers');
 
+// Create an apollo server instance based on the type definitions and the resolvers 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
+// Attach apollo server to the express app. Path is the endpoint of the GraphQL AP 
 apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,23 @@
 const db = require('./database');
 
 const Query = {
-    users: () => {
-        return db.users.list();
-    },
-    user: (root, { id }) => db.users.get(id)
+    users: () => db.users.list(),
+    user: (root, { id }) => db.users.get(id),
+    tweets: (root, { userId, last }) => {
+        const tweets = db.tweets.list().filter(tweet => tweet.userId == userId);
+        return last ? tweets.slice(-last) : tweets;
+    }
 };
 
-module.exports = { Query };
+const User = {
+    followers: (user) => {
+        const followersIds = db.users.get(user.id).followers;
+        const followers = [];
+        for (id of followersIds) {
+            followers.push(db.users.get(id));
+        }
+        return followers;
+    }
+}
+
+module.exports = { Query, User };
